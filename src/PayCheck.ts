d3a383f6351f67f6a5e8f7aff788f5669cf37850
Steve Mu <skycloud112@gmail.com>
2024-03-14 19:58:21 -0400

    remove .ts from Hold

--- src/PayCheck.ts
+++ src/PayCheck.ts
@@ -2,7 +2,7 @@ export class PayCheck {
   public grossPay: number = 0;
   public deductions: number = 0;
   public netPay: number = 0;
-  public disposition: string = 'Hold.ts';
+  public disposition: string = 'Hold';
 
   constructor(
     public payPeriodStartDate: Date,
