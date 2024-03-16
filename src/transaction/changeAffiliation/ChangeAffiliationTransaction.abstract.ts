dd45d90d3d4d0f701abccb8ea7304a9e67116fad
Steve Mu <skycloud112@gmail.com>
2024-03-16 16:57:20 -0400

    add async and await

--- src/transaction/changeAffiliation/ChangeAffiliationTransaction.abstract.ts
+++ src/transaction/changeAffiliation/ChangeAffiliationTransaction.abstract.ts
@@ -3,12 +3,12 @@ import { Affiliation } from '../../affiliation/Affiliation.interface.ts';
 import { ChangeEmployeeTransaction } from '../changeEmployee/ChangeEmployeeTransaction.abstract.ts';
 
 export abstract class ChangeAffiliationTransaction extends ChangeEmployeeTransaction {
-  abstract recordMembership(employee: Employee): void;
+  abstract recordMembership(employee: Employee): Promise<void>;
 
   abstract getAffiliation(): Affiliation;
 
-  change(employee: Employee): void {
-    this.recordMembership(employee);
+  async change(employee: Employee): Promise<void> {
+    await this.recordMembership(employee);
     employee.affiliation = this.getAffiliation();
   }
 }
